64c3730174aef13efc0cd80283ce8af18a92f308
Alfonso Robles <fons3517@gmail.com>
2021-10-11 09:59:09 -0500

    Added server.js file. Working on connecting pages and building wildcard route to 404 Error

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const api = require('./assets/index.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+//GET Route for homepage
+app.get('/', (req, res) => 
+res.send('Navigate to /index.html or /notes.html')
+);
+
+//GET Route for index.html
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, '/index.html'))
+);
+
+
+//GET Route for notes.html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+console.log( `App listening at http://localhost:${PORT}`)
+);
